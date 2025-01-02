1b40fe947586c6b465ec4c0308993d71b827d7ce
Udaya1999 <udayabhaskar209@gmail.com>
2025-01-02 21:36:10 +0530

    Removed node_modules

--- src/app.js
+++ src/app.js
@@ -8,9 +8,15 @@ app.use("/test", (req, res) => {
 });
 app.use("/hello", (req, res) => {
   // res.setEncoding("utf8 "); // Use a valid encoding
-  res.send("Hello hello hello");
+  res.send("Hello hello hello ");
 });
 
 app.listen(3000, () => {
   console.log(colors.rainbow("Dev-Server_started......!!"));
 });
+
+// git init
+// >> git commit -m "second commit"
+// >> git branch -M main
+// >> git remote add origin https://github.com/Udaya1999/devTinder.git
+// >> git push -u origin main
