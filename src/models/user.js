9f2b56de7311a0129b154e94f0c492d2e13988db
Udaya1999 <udayabhaskar209@gmail.com>
2025-01-04 11:45:00 +0530

    validator npm package to check validations

--- src/models/user.js
+++ src/models/user.js
@@ -1,52 +1,73 @@
 const mongoose = require("mongoose");
-const userSchema = new  mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true,
-        minLength:4,
-        maxLength:20,
+const validator = require('validator');
+const { trim } = require("validator");
+const { default: isURL } = require("validator/lib/isURL");
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 20,
     },
-    lastName:{
-        type:String,
-        minLength:10,
-        maxLength:30,
+    lastName: {
+      type: String,
+      minLength: 4,
+      maxLength: 20,
     },
-    emailId:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true,
-
-        
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    age:{
-        type:Number,
-        min:18,  
-    },
-    gender:{
-        type:String,
-        enum:["Male","Female","Other"],
-        validate(value){
-            if(!["Male","Female","Others"].includes(value)){
-                throw new Error("Invalid gender");
-            }
+    emailId: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+        validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email Id"+ value);
         }
+      },
     },
-    photoUrl:{
-        type:String,
-        default:"https://tse3.mm.bing.net/th?id=OIP.w0TcjC4y9CxTrY3sitYa_AAAAA&pid=Api&P=0&h=180",
+    password: {
+      type: String,
+      required: true,
+      validator(value){
+        if(!validator.isStrongPassword(value)){
+            throw new Error("Enter a strong password "+ value)
+        }
+      }
     },
-    about:{
-        type:String,
-        default: "This is a default description about the user"
+    age: {
+      type: Number,
+      min: 18,
     },
-    skills:{
-        type:[String],
-    }
-},{ timestamps :true});
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "Other"],
+      validate(value) {
+        if (!["Male", "Female", "Others"].includes(value)) {
+          throw new Error("Invalid gender" );
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://tse3.mm.bing.net/th?id=OIP.w0TcjC4y9CxTrY3sitYa_AAAAA&pid=Api&P=0&h=180",
+      validate(value) {
+        if (!isURL(value)) {
+          throw new Error("Invalid photo URL...");
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is a default description about the user",
+    },
+    skills: {
+      type: [String],
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("user",userSchema);
+module.exports = mongoose.model("user", userSchema);
