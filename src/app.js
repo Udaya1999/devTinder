afeb12944f29a027cf627d33ab33e61810105bc1
Udaya1999 <udayabhaskar209@gmail.com>
2025-01-04 22:19:59 +0530

    schema modeles

--- src/app.js
+++ src/app.js
@@ -3,11 +3,9 @@ const colors = require("colors");
 const connectDB = require("./config/dataBase");
 const app = express();
 const User = require("./models/user");
-const { Error } = require("mongoose");
 const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");
 const userAuth = require("./middlewares/auth");
 
 app.use(express.json()); // middleware for json data
@@ -15,14 +13,14 @@ app.use(cookieParser()); // middleware for cookies (JWT - tokens...)
 
 app.post("/signup", async (req, res) => {
   try {
-    //validation of data
+    // Validation of data
     validateSignUpData(req);
 
-    //encript the password
+    // Encrypt the password
     const { password, firstName, lastName, emailId, age } = req.body;
     const passwordHash = await bcrypt.hash(password, 10);
 
-    // new instance of the object
+    // New instance of the object
     const user = new User({
       firstName,
       lastName,
@@ -31,26 +29,26 @@ app.post("/signup", async (req, res) => {
       age,
     });
     await user.save();
-    res.send("user created");
+    res.send("User created");
   } catch (err) {
     res.status(400).send("Error saving the user: " + err.message);
   }
 });
+
 app.post("/login", async (req, res) => {
   try {
     const { emailId, password } = req.body;
-    const user = await User.findOne({ emailId });
+    const user = await User.findOne({ emailId: emailId });
     if (!user) {
       throw new Error("Invalid credentials");
     }
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isPasswordValid = await user.validatePassword(password);
     if (isPasswordValid) {
-      // create a jwt token
-      const token = await jwt.sign({ _id: user._id }, "UDAY@Tinder$209",{expiresIn: "1d"});
+      // Create a JWT token
+      const token = await user.getJWT();
 
       // Add the token to cookie and send the response back to the user
       res.cookie("token", token);
-
       res.send("User logged in");
     } else {
       throw new Error("Invalid credentials");
@@ -71,17 +69,17 @@ app.get("/profile", userAuth, async (req, res) => {
 
 app.post("/sendConnectionRequest", userAuth, async (req, res) => {
   const user = req.user;
-  console.log("sending a connection request");
-  res.send( user.firstName + " sent you a Connection Request");
+  console.log("Sending a connection request");
+  res.send(`${user.firstName} sent you a Connection Request`);
 });
 
 connectDB()
   .then(() => {
-    console.log("db connected successfully ...!");
+    console.log("DB connected successfully ...!");
     app.listen(3000, () => {
       console.log(colors.rainbow("Dev-Server started......!!"));
     });
   })
   .catch((err) => {
-    console.error("db is not connected");
+    console.error("DB is not connected");
   });
--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const { default: isURL } = require("validator/lib/isURL");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const userSchema = new mongoose.Schema(
   {
     firstName: {
@@ -22,16 +24,16 @@ const userSchema = new mongoose.Schema(
       lowercase: true,
       validate(value) {
         if (!validator.isEmail(value)) {
-          throw new Error("Invalid Email Id" + value);
+          throw new Error("Invalid Email Id: " + value);
         }
       },
     },
     password: {
       type: String,
       required: true,
-      validator(value) {
+      validate(value) {
         if (!validator.isStrongPassword(value)) {
-          throw new Error("Enter a strong password " + value);
+          throw new Error("Enter a strong password: " + value);
         }
       },
     },
@@ -42,18 +44,13 @@ const userSchema = new mongoose.Schema(
     gender: {
       type: String,
       enum: ["Male", "Female", "Other"],
-      validate(value) {
-        if (!["Male", "Female", "Others"].includes(value)) {
-          throw new Error("Invalid gender");
-        }
-      },
     },
     photoUrl: {
       type: String,
       default:
         "https://tse3.mm.bing.net/th?id=OIP.w0TcjC4y9CxTrY3sitYa_AAAAA&pid=Api&P=0&h=180",
       validate(value) {
-        if (!isURL(value)) {
+        if (!validator.isURL(value)) {
           throw new Error("Invalid photo URL...");
         }
       },
@@ -69,4 +66,19 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("user", userSchema);
+userSchema.methods.getJWT = async function () {
+  const user = this;
+  const token = await jwt.sign({ _id: user._id }, "UDAY@Tinder$209", {
+    expiresIn: "1d",
+  });
+  return token;
+};
+
+userSchema.methods.validatePassword = async function (passInpUser) {
+  const user = this;
+  const passwordHash = user.password;
+  const isPasswordValid = await bcrypt.compare(passInpUser, passwordHash); // Use passInpUser variable
+  return isPasswordValid;
+};
+
+module.exports = mongoose.model("User", userSchema); // Export as User
